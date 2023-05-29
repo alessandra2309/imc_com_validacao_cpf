ce9b6b9815c18f60a6b3df3ac55427bc5555d496
alessandra Marques <alessandra2309@gmail.com>
2023-05-29 17:14:28 -0400

    meu primeiro commit de poo

---
+++ aula2/Program.cs
@@ -0,0 +1,8 @@
+﻿Aluno aluno1 = new Aluno();
+aluno1.nome = "alessandra";
+aluno1.email = "email";
+aluno1.notafinal = 8;
+aluno1.notafinal2 = 9;
+
+aluno1.calcularMedia(8.5,9);
+
---
+++ aula2/aluno.cs
@@ -0,0 +1,29 @@
+﻿public class Aluno
+{
+   public string nome;
+    public string email;
+    public double notafinal;
+    public double notafinal2;
+
+    public void calcularMedia(double nota1,double nota2)
+    {
+        double media = (notafinal + notafinal2)/2;
+        Console.WriteLine($"A média é {media}");
+
+        if (media < 6)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("REPROVADO");
+            Console.ResetColor();
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("PASSOU");
+            Console.ResetColor();
+        }
+    }
+
+    
+
+}
